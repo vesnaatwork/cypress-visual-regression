d3c8a30199da368f0633cc86a10a8eb4e69152c5
Vesna Petkovic <vesna@work.co>
2024-10-24 17:06:30 +0200

    Add heights for all breakpoints

--- cypress/config.js
+++ cypress/config.js
@@ -2,14 +2,14 @@
 module.exports = {
     scenarios: [
       { cookiesAccepted: false, description: 'with-cookies' },
-      { cookiesAccepted: true, description: 'without-cookies' }
+      // { cookiesAccepted: true, description: 'without-cookies' }
     ],
   
     breakpoints: [
-      { name: 'S', width: 375,height:800 },
-      { name: 'M', width: 768 },
-      { name: 'L', width: 1024 },
-      { name: 'XL', width: 1440 },
+      { name: 'S', width: 375,height:700 },
+      { name: 'M', width: 768, height:1000 },
+      { name: 'L', width: 1024, height: 1100},
+      { name: 'XL', width: 1440, height:1200 },
       { name: 'XXL', width: 2560, height:1920 }
     ],
     pages : [
